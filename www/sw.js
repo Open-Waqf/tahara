da879b1d6387846e159bbedfc1811f34bd99ba47
Open Waqf Contributor <contact@open-waqf.org>
2026-01-29 22:46:22 +0100

    Deploy

--- www/sw.js
+++ www/sw.js
@@ -9,6 +9,12 @@ const ASSETS = [
     "manifest.json",
     "./fonts/amiri-v30-arabic_latin-700.woff2",
     "./fonts/amiri-v30-arabic_latin-regular.woff2",
+    "./img/favicon.ico",
+    "./img/favicon.svg",
+    "./img/favicon-96x96.png",
+    "./img/apple-touch-icon.png",
+    "./img/web-app-manifest-192x192.png",
+    "./img/web-app-manifest-512x512.png",
 ];
 
 self.addEventListener("install", (event) => {
