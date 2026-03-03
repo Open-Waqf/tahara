2e01173a5fa7cb666c529630ef75a3f0cf3f18e3
Open Waqf Contributor <contact@open-waqf.org>
2026-03-03 13:47:35 +0100

    guidelines

--- www/rules.js
+++ www/rules.js
@@ -7,6 +7,7 @@ export const FiqhRules = {
     'hanafi': {
         madhhab: 'hanafi',
         version: '1.0',
+        sourceKey: 'hanafi_source',
         rules: {
             minHaydMs: 3 * 24 * 60 * 60 * 1000,      // 3 days
             maxHaydMs: 10 * 24 * 60 * 60 * 1000,     // 10 days
@@ -17,6 +18,7 @@ export const FiqhRules = {
     'shafi': {
         madhhab: 'shafi',
         version: '1.0',
+        sourceKey: 'shafi_source',
         rules: {
             minHaydMs: 24 * 60 * 60 * 1000,          // 24 hours (1 day)
             maxHaydMs: 15 * 24 * 60 * 60 * 1000,     // 15 days
